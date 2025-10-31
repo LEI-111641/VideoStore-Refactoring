312e8e62ed8ae7dafad358beb7828d3344d2a592
LEI-124300 <mjfca2@Iscte-iul.pt>
2025-10-31 19:12:28 +0000

    V9 - Replace Conditional with Polymorphism (State Pattern applied to Price hierarchy)

--- src/semana4/Main.java
+++ src/semana4/Main.java
@@ -1,5 +1,17 @@
 package semana4;
 
+import v9.ChildrensPrice;
+import v9.Customer;
+import v9.Movie;
+import v9.NewReleasePrice;
+import v9.Price;
+import v9.RegularPrice;
+import v9.Rental;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /***********************************************************
  * Filename: Main.java
  * @author fba 6 de Mai de 2013
@@ -10,22 +22,24 @@ public abstract class Main
 	/***********************************************************
 	 * @param args
 	 ***********************************************************/
-	public static void main(String[] args)
-	{
-        Price regularPrice = new Price(Price.Code.REGULAR);
-        Price childrensPrice = new Price(Price.Code.CHILDRENS);
-        Price newReleasePrice = new Price(Price.Code.NEW_RELEASE);
+	public static void main(String[] args) throws IOException {
+        v9.Price regularPrice = new RegularPrice();
+        v9.Price childrensPrice = new ChildrensPrice();
+        Price newReleasePrice = new NewReleasePrice();
 
-        Customer who = new Customer("Barack Obama");
-        Movie m1 = new Movie("Life of Amalia", regularPrice);
-        Movie m2 = new Movie("Peter Pan", childrensPrice);
-        Movie m3 = new Movie("Donna del Lago", newReleasePrice);
+        v9.Customer who = new Customer("Barack Obama");
+        v9.Movie m1 = new v9.Movie("Life of Amalia", regularPrice);
+        v9.Movie m2 = new v9.Movie("Peter Pan", childrensPrice);
+        v9.Movie m3 = new Movie("Donna del Lago", newReleasePrice);
 
-        who.addRental(new Rental(m1, 1));
-        who.addRental(new Rental(m2, 2));
+        who.addRental(new v9.Rental(m1, 1));
+        who.addRental(new v9.Rental(m2, 2));
         who.addRental(new Rental(m3, 3));
+        System.out.println( who.statement());
 
-        System.out.println(who.statement());;
+        PrintWriter html = new PrintWriter(new FileWriter("webPages/statement.html"));
+        html.println(who.htmlStatement());
+        html.close();
 	}
 
 }
--- src/semana4/Price.java
+++ src/semana4/Price.java
@@ -1,38 +1,10 @@
 package semana4;
 
-public class Price {
-    public enum Code {REGULAR, CHILDRENS, NEW_RELEASE}
-    private Code _code;
+public abstract class Price {
 
-    public Price(Code code) {_code = code;
-    }
-    public Code getCode() {return _code;
-    }
-    public int getFrequentRentalPoints(int duration) {
-        return ((_code == Code.NEW_RELEASE) && duration > 1) ? 2 : 1;
-    }
 
-    public double getRentalAmount(int duration) {
-        double thisAmount = 0;
+    public abstract int getFrequentRentalPoints(int duration);
 
-        // determine amounts for each line
-        switch (_code) {
-            case REGULAR:
-                thisAmount += 2;
-                if (duration > 2)
-                    thisAmount += (duration - 2) * 1.5;
-                break;
-            case NEW_RELEASE:
-                thisAmount += duration * 3;
-                break;
-            case CHILDRENS:
-                thisAmount += 1.5;
-                if (duration > 3)
-                    thisAmount += (duration - 3) * 1.5;
-                break;
-        }
-        return thisAmount;
-    }
-
+    public abstract double getRentalAmount(int duration);
 
 }
